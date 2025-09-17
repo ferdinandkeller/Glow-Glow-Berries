2b13861cfbdb4744fb96bd39b7fd0d6dda2e865f
Ferdinand Keller <ferdinand.keller@proton.me>
2025-09-17 21:59:40 +0200

    support for 1.21

--- src/main/java/dev/ferdinandkeller/glowglowberries/mixin/Items.java
+++ src/main/java/dev/ferdinandkeller/glowglowberries/mixin/Items.java
@@ -1,35 +1,42 @@
 package dev.ferdinandkeller.glowglowberries.mixin;
 
-import net.minecraft.component.type.ConsumableComponent;
-import net.minecraft.component.type.ConsumableComponents;
-import net.minecraft.component.type.FoodComponents;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
-import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(net.minecraft.item.Items.class)
 public class Items {
-	@Inject(at = @At("HEAD"), method = "register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/item/Item$Settings;)Lnet/minecraft/item/Item;")
-	private static void register(String id, Function<Item.Settings, Item> factory, Item.Settings settings, CallbackInfoReturnable<Item> cir) {
-        if (!id.equals("glow_berries")) return;
+    @ModifyArgs(
+        method = "<clinit>",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/item/AliasedBlockItem;<init>(Lnet/minecraft/block/Block;Lnet/minecraft/item/Item$Settings;)V"
+        )
+    )
+    private static void register(Args args) {
+        // find which block we are applying the mixin to
+        Block block = (Block) args.get(0);
 
-        // create a custom Glow Berries consumable component
-        ConsumableComponent GLOW_BERRIES = ConsumableComponents.food()
-            .consumeEffect(
-                new ApplyEffectsConsumeEffect(
-                    new StatusEffectInstance(StatusEffects.GLOWING, 40)
-                )
-            )
+        if (!block.equals(Blocks.CAVE_VINES)) return;
+
+        Item.Settings settings = (Item.Settings) args.get(1);
+
+        // create a custom Glow Berries food component
+        FoodComponent GLOW_BERRIES = new FoodComponent.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1F)
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 40), 1.0F)
             .build();
 
         // update the settings
-        settings.food(FoodComponents.GLOW_BERRIES, GLOW_BERRIES);
-	}
+        settings.food(GLOW_BERRIES);
+    }
 }
+
